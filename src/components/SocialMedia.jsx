d7ba8ba4b6e428a3c2c7f7c401a7d877930bbdd0
Saad1379 <sheikh.saad.riaz@gmail.com>
2023-03-20 23:37:17 +0500

    first commit

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BsLinkedin, BsInstagram } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsLinkedin />
+    </div>
+    <div>
+      <MdEmail />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
