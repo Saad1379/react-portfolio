44a719afd56634e3d14f20a7848c6b38876a9c8a
saad1379 <sheikh.saad.riaz@gmail.com>
2023-03-24 01:34:21 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 import Sitemap from "./utils/Sitemap";
+
 const App = () => (
   <div className="app">
     <Navbar />
